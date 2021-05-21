a5b6fc9b44eaa6f5919587acf2869a237df16ebf
Julian Stephens <julianstephens55@gmail.com>
2021-05-21 11:10:14 -0400

    Update logs with probe info

--- experiment.py
+++ experiment.py
@@ -1,5 +1,10 @@
 import pandas as pd
 
+#  Experiment defaults
+default_duration = 0.0
+default_keys = ["d", "k"]
+default_text_height = 0.1
+
 #  Introduction
 intro = """YOU WILL NOW VIEW ANOTHER SERIES OF WORDS.  THESE ARE THE SAME WORDS YOU WERE ASKED TO 
 REMEMBER DURING THE PREVIOUS SEGMENTS.
@@ -14,12 +19,15 @@ SO PLEASE TAKE YOUR TIME AND BE AS ACCURATE AS
 POSSIBLE. 
 
 PLEASE PRESS D or K WHEN YOU ARE READY TO BEGIN."""
-intro_duration = 2.0
-intro_key = ["d", "k"]
+intro_text_height = 0.05
 
-#  Random word key assignments
-rand_odd_key = "'D' = BUILDING, 'K' = FACE"
-rand_even_key = "'D' = FACE, 'K' =  BUILDING"
+#  Uncomment below to set separate defaults for introduction
+#  intro_duration = 0.0
+#  intro_key = ["d", "k"]
+
+#  Noun key assignments
+noun_odd_key = "'D' = BUILDING, 'K' = FACE"
+noun_even_key = "'D' = FACE, 'K' =  BUILDING"
 
 #  Probes
 house_probe_1 = "IS THIS A RESIDENTIAL OR COMMERCIAL BUILDING?"
@@ -29,7 +37,7 @@ face_probe_1 = "IS THIS A MALE FACE OR A FEMALE FACE?"
 face_probe_2 = "IS THIS A YOUNGER PERSON OR AN OLDER PERSON?"
 face_probe_3 = "IS THIS PERSON WHITE OR NOT WHITE?"
 
-#  Key assignments for ODD users
+#  Probe key assignments for ODD users
 house_key_odd_1 = ["'D' = RESIDENTIAL, 'K' = COMMERCIAL"]
 house_key_odd_2 = ["'D' = ONE STORY, 'K' = MULTISTORY"]
 house_key_odd_3 = ["'D' = LIGHT, 'K' = DARK"]
@@ -37,7 +45,7 @@ face_key_odd_1 = ["'D' = MALE, 'K' = FEMALE"]
 face_key_odd_2 = ["'D' = YOUNGER, 'K' = OLDER"]
 face_key_odd_3 = ["'D' = WHITE, 'K' = NOT WHITE"]
 
-#  Key assignments for EVEN users
+#  Probe key assignments for EVEN users
 house_key_even_1 = ["'D' = COMMERCIAL, 'K' = RESIDENTIAL"]
 house_key_even_2 = ["'D' = MULTISTORY, 'K' = ONE STORY"]
 house_key_even_3 = ["'D' = DARK, 'K' = LIGHT"]
@@ -45,6 +53,7 @@ face_key_even_1 = ["'D' = FEMALE, 'K' = MALE"]
 face_key_even_2 = ["'D' = OLDER, 'K' = YOUNGER"]
 face_key_even_3 = ["'D' = NOT WHITE, 'K' = WHITE"]
 
+#  ----DO NOT MODIFY BELOW----
 house_list_odd = [(house_probe_1, house_key_odd_1), (house_probe_2,
                                                      house_key_odd_2), (house_probe_3, house_key_odd_3)]
 house_list_even = [(house_probe_1, house_key_even_1), (house_probe_2,
@@ -53,7 +62,7 @@ face_list_odd = [(face_probe_1, face_key_odd_1), (face_probe_2,
                                                   face_key_odd_2), (face_probe_3, face_key_odd_3)]
 face_list_even = [(face_probe_1, face_key_even_1), (face_probe_2,
                                                     face_key_even_2), (face_probe_3, face_key_even_3)]
-
+#  ----DO NOT MODIFY ABOVE----
 
 #  Load stim list and get 40 random rows
 excel_df = pd.read_excel('./Stim_AssociateMasterList.xlsx',
@@ -61,6 +70,7 @@ excel_df = pd.read_excel('./Stim_AssociateMasterList.xlsx',
 face_rows = excel_df[excel_df.ASSOCIATE == "Face"].sample(20)
 house_rows = excel_df[excel_df.ASSOCIATE == "House"].sample(20)
 
-#  Create log
+#  Location of log files
 log_df = pd.DataFrame(columns=['NOUN', 'ASSOCIATE', 'MF_RC', 'YO_OM', 'WN_LD', 'RESP_KEY', 'RESP_CODED',
                                'RESP_CORRECT', 'RESP_TIME', 'PROBE_TYPE', 'PROBE_KEY', 'PROBE_CORRECT', 'PROBE_TIME'])
+log_dir = "logs"
--- stimulus.py
+++ stimulus.py
@@ -1,8 +1,9 @@
 from collections import OrderedDict
 import ctypes
-import random
-import sys
 import os
+import random
+import re
+import sys
 
 import pandas as pd
 from psychopy import core, event, logging, visual
@@ -144,7 +145,7 @@ class Paradigm:
             return stim.show(is_odd)
         else:
             elapsed = experiment_timer.getTime()
-            par.log_data["exp_runtime"] = ["Total Experiment Runtime", "", "", "", "", "", "", "", "", elapsed] 
+            par.log_data["exp_runtime"] = ["Total Experiment Runtime", "", "", "", "", "", "", "", "", "", "", "", "", "", elapsed] 
 
             #  Get participant ID and format
             uid = settings["participant"]
@@ -152,14 +153,14 @@ class Paradigm:
                 uid = "0" + str(uid) 
 
             #  Create logs directory if doesn't exist
-            if not os.path.exists('logs'):
-                os.makedirs('logs')
+            if not os.path.exists(ex.log_dir):
+                os.makedirs(ex.log_dir)
 
             #  Dump log info to csv
-            log_path = "logs/" + str(uid) + "_posttest_log.csv"
+            log_path = str(ex.log_dir) + "/" + str(uid) + "_posttest_log.csv"
             log_df = pd.DataFrame.from_dict(
                 par.log_data, orient='index', columns=['NOUN', 'ASSOCIATE', 'MF_RC', 'YO_OM', 'WN_LD', 'RESP_KEY', 'RESP_CODED',
-                                                          'RESP_CORRECT', 'KEY_RT', 'PROBE_TYPE'])
+                                                          'RESP_CORRECT', 'KEY_RT', 'PROBE_TYPE', 'PROBE', 'PROBE_KEY', 'PROBE_CODED', 'PROBE_CORRECT', 'PROBE_RT'])
             log_df.to_csv(log_path, index=False)
 
             core.quit()
@@ -199,7 +200,7 @@ class Stimulus(object):
 
 
 class Text(Stimulus):
-    def __init__(self, window, text, height=0.1, duration=2.0, keys=None, is_probe=False):
+    def __init__(self, window, text, height, duration, keys, stim_type, probe_details=None):
         """Initializes a text stimulus
 
         Args:
@@ -207,7 +208,9 @@ class Text(Stimulus):
             text: text to display
             duration: the duration the text will appear
             keys: the list of keys to press to continue to the next stimulus (if None, will automatically go to next stimulus)
-            is_probe: T/F if stimulus is a word or probe
+            stim_type: float 0 if probe, 1 if intro, -1 otherwise
+            probe_details: dict probe question and answer key 
+
         """
         self.window = window
         self.word_key = list(text.keys())[0]
@@ -216,19 +219,20 @@ class Text(Stimulus):
             self.window, text=text[self.word_key], height=self.height, units="norm")
         self.duration = duration
         self.keys = keys
-        self.is_probe = is_probe
+        self.stim_type = stim_type
+        self.probe_details = probe_details
 
     def show(self, is_odd):
         self.text.draw()
         self.window.flip()
-        #  TODO: Create clock
+
+        #  Create timer for individual stimulus
         stim_timer = core.MonotonicClock()
 
         if self.duration:
             core.wait(self.duration)
         elif self.keys:
-            wait = WaitForKey(self.window, self.keys,
-                              self.word_key, self.is_probe)
+            wait = WaitForKey(self.window, self.keys, self.word_key, self.stim_type, self.probe_details)
 
             return wait.show(self, is_odd, stim_timer)
 
@@ -242,6 +246,7 @@ class Pause(Stimulus):
     Attributes:
         duration
         window
+
     """
 
     def __init__(self, window, duration):
@@ -261,15 +266,17 @@ class WaitForKey(Stimulus):
         window
         keys
         word_key
+        stim_type
 
     """
 
-    def __init__(self, window, keys, word_key, is_probe, event="continue"):
+    def __init__(self, window, keys, word_key, stim_type, probe_details, event="continue"):
         self.window = window
         self.keys = keys
         self.event = event
         self.word_key = word_key
-        self.is_probe = is_probe
+        self.stim_type = stim_type
+        self.probe_details = probe_details
 
     def show(self, stimulus, is_odd, stim_timer):
         #  Get participant answer
@@ -277,11 +284,11 @@ class WaitForKey(Stimulus):
         key_rt = stim_timer.getTime() 
 
         #  Process answer
-        self.run_event(stimulus, is_odd, self.is_probe, key_pressed, key_rt)
+        self.run_event(stimulus, is_odd, self.stim_type, self.probe_details, key_pressed, key_rt)
 
         return self
 
-    def run_event(self, stimulus, is_odd, is_probe, key_pressed, key_rt):
+    def run_event(self, stimulus, is_odd, stim_type, probe_details, key_pressed, key_rt):
         global par
 
         if self.event == "exit":
@@ -289,12 +296,30 @@ class WaitForKey(Stimulus):
             self.window.close()
             core.quit()
 
-        #  TODO: Add probe data to log
-        #  if is_probe:
-        #      last = next(reversed(par.log_data.keys()))
-        #      log = par.log_data[last]
-        #      #  log.append([key_pressed, ])
-        if not is_probe:
+        if stim_type == 0:
+            #  Get noun that came before probe
+            last_noun = next(reversed(par.log_data.keys()))
+
+            #  Get potential probe answers
+            mf_rc = ex.excel_df.loc[ex.excel_df.NOUN ==
+                                    last_noun, 'MF_RC'].values[0]
+            yo_om = ex.excel_df.loc[ex.excel_df.NOUN ==
+                                    last_noun, 'YO_OM'].values[0]
+            wn_ld = ex.excel_df.loc[ex.excel_df.NOUN ==
+                                    last_noun, 'WN_LD'].values[0]
+            last_noun_attributes = [mf_rc.upper(), yo_om.upper(), wn_ld.upper()]
+
+            #  Process user response
+            probe_question = probe_details['q']
+            probe_answer = probe_details[key_pressed].upper()
+
+            correct_flag = 1 if probe_answer in last_noun_attributes else 0
+
+            #  Update log with probe info
+            log = par.log_data[last_noun]
+            log.extend([probe_question, key_pressed, probe_answer, correct_flag, key_rt])
+            par.log_data[last_noun] = log
+        elif stim_type < 0:
             #  Create key for answer lookup
             answer_code = dict()
 
@@ -307,12 +332,11 @@ class WaitForKey(Stimulus):
                 answer_code['k'] = 'f'
 
             #  Get correct answer
-            correct_answer = ex.excel_df.loc[ex.excel_df.NOUN ==
-                                             self.word_key, 'recallRespCorrect'].values[0]
-            word_type = ex.excel_df.loc[ex.excel_df.NOUN ==
-                                        self.word_key].ASSOCIATE.values[0]
+            correct_answer = ex.excel_df.loc[ex.excel_df.NOUN == self.word_key, 'recallRespCorrect'].values[0]
+            word_type = ex.excel_df.loc[ex.excel_df.NOUN == self.word_key].ASSOCIATE.values[0]
             probe_type = word_type.upper()
 
+            #  Get misc info related to word from master list
             mf_rc = ex.excel_df.loc[ex.excel_df.NOUN ==
                                     self.word_key, 'MF_RC'].values[0]
             yo_om = ex.excel_df.loc[ex.excel_df.NOUN ==
@@ -341,6 +365,7 @@ def wait_for_key(keys):
 
     Args:
         keys: a list or tuple of keys
+
     """
     event.clearEvents()
 
@@ -351,13 +376,13 @@ def get_probe(probe_type, is_odd):
     """Insert probe at beginning of master stim list 
 
     Args:
-        probe_type: Face or House probe
-        is_odd: T/F participant ID is odd
+        probe_type: str Face or House probe
+        is_odd: bool participant ID is odd
 
     """
     probe_tup = None
 
-    #  Get probe
+    #  Get probe based on type (house or face) and is_odd
     if probe_type == 'HOUSE' and is_odd:
         probe_tup = random.choice(ex.house_list_odd)
     elif probe_type == 'HOUSE' and not is_odd:
@@ -370,10 +395,23 @@ def get_probe(probe_type, is_odd):
     if probe_tup:
         (probe, keys) = probe_tup
 
+        #  Extract answer key for check later
+        split = keys[0].split(",")
+        d_key = split[0]
+        k_key = split[1]
+
+        d_answer = d_key[d_key.index('='):][2:] 
+        k_answer = k_key[k_key.index('='):][2:] 
+        probe_details = {
+            'q': probe,
+            'd': d_answer,
+            'k': k_answer
+        }
+
         #  Construct stim and add to stimlist
         display_text = dict()
         display_text[probe] = probe + "\n" + keys[0]
-        stim = (Text, (display_text, 0.1, 0.0, ["d", "k"], True))
+        stim = (Text, (display_text, ex.default_text_height, ex.default_duration, ex.default_keys, 0, probe_details))
         insert(stim)
 
     else:
@@ -386,32 +424,31 @@ def constructPar(is_odd):
     """ Initializes experiment paradigm
 
     Args:
-        is_odd: T/F participant ID is odd
+        is_odd: bool if participant ID is odd
 
     """
     global par
-    par = Paradigm(
-        window_dimensions=settings["window_dimensions"], escape_key="escape")
-    default_duration = 2.0
-    default_keys = ["d", "k"]
+    par = Paradigm(window_dimensions=settings["window_dimensions"], escape_key="escape")
 
-    #  Get list of 40 random words
+    #  Get list of 40 random nouns
     face_words = ex.face_rows.NOUN.tolist()
     house_words = ex.house_rows.NOUN.tolist()
     random_words = face_words + house_words
     random.shuffle(random_words)
 
-    #  Create intro routine
+    #  Create intro stimulus
     intro_text = dict()
     intro_text['intro'] = ex.intro
-    stimuli = [(Text, (intro_text, 0.05, ex.intro_duration, ex.intro_key))]
+
+    #  To use separate defaults for intro change below to ex.intro_duration and ex.intro_keys
+    stimuli = [(Text, (intro_text, ex.intro_text_height, ex.default_duration, ex.default_keys, 1))]
 
     #  Create word stimuli
     for word in random_words:
-        key_text = ex.rand_odd_key if is_odd else ex.rand_even_key
+        key_text = ex.noun_odd_key if is_odd else ex.noun_even_key
         display_text = dict()
         display_text[word] = word + "\n" + key_text
-        stim = (Text, (display_text, 0.1, 0.0, default_keys))
+        stim = (Text, (display_text, ex.default_text_height, ex.default_duration, ex.default_keys, -1))
         stimuli.append(stim)
 
     #  Add stimuli to paradigm
